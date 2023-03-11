9dbbbbda625284d30842feaa1b346e245cb105ee
former_economist <adam.smith.2021@uni.strath.ac.uk>
2023-03-11 20:26:46 +0000

    completed navigation

---
+++ printStore/src/pages/Workpage/WorkPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './WorkPage.module.css'
+
+const WorkPage = () => {
+  
+  return (
+    <main className={`${styles.main} ${styles.grid}`}>
+      <div>WorkPage</div>
+    </main>
+  )
+}
+
+export default WorkPage
